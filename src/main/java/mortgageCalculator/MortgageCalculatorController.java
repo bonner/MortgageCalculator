5b7fb2568420d1a6fbeb6975faa96c094b2aceae
Michael Bonner <mike.bonner@ge.com>
2019-06-01 15:50:34 -0700

    added root mapping, debugging heroku deployment

--- src/main/java/mortgageCalculator/MortgageCalculatorController.java
+++ src/main/java/mortgageCalculator/MortgageCalculatorController.java
@@ -13,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -37,6 +38,12 @@ public class MortgageCalculatorController {
     }
     protected static ResponseEntity<Map<?, ?>> resp(HttpStatus status, Map<?, ?> map) {
         return new ResponseEntity<Map<?, ?>>(map, status);
+    }
+    
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello World!";
     }
     
 
